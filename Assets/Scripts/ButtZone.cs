9ef39bce3fc8639be52e0a9d56f85858b2397afc
B1B1DEV <chabiron.olivier@gmail.com>
2016-04-16 16:59:41 +0200

    chariot, slaves and zones

---
+++ Assets/Scripts/ButtZone.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ButtZone : Zone
+{
+    public Collider zoneCollider;
+
+    override void OnTriggerEnter(Collider slaveCollider)
+    {
+        SlaveController sc = slaveCollider.gameObject.GetComponent<SlaveController>();
+        if (sc != null)
+        {
+            chariot.SendSlaveToWork(sc);
+        }
+    }
+
+}
---
+++ Assets/Scripts/ChariotController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChariotController : MonoBehaviour {
+
+    public GameObject chariotGO;
+    public Vector2 chariotPosition;
+    private float forwardSpeed;
+    private float lateralSpeed;
+
+    public Collider buttZone;
+    public Collider leftZone;
+    public Collider rightZone;
+
+    public int leftWorkForce;
+    public int rightWorkForce;
+
+    public Vector2 initialPosition;
+
+    // Use this for initialization
+
+    void Start ()
+    {
+        chariotPosition = initialPosition;
+        forwardSpeed = 0f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        // Evaluate Chariot Speed
+        forwardSpeed = leftWorkForce + rightWorkForce;
+        lateralSpeed = rightWorkForce - leftWorkForce;
+
+        if (forwardSpeed > 100)
+        {
+            forwardSpeed = 100;
+        }
+
+        if (lateralSpeed > 50)
+        {
+            lateralSpeed = 50;
+        }
+        else if (lateralSpeed < -50)
+        {
+            lateralSpeed = -50;
+        }
+
+        // Move Chariot according to speed  
+        Vector2 velocity = new Vector2(forwardSpeed / 10f, lateralSpeed / 10f);
+        MoveChariot(velocity);
+	}
+ 
+    void MoveChariot(Vector2 chariotDirection)
+    {
+        chariotPosition += chariotDirection;
+        chariotGO.transform.position = chariotPosition;
+    }
+
+    void SendSlaveToWork(SlaveController sc)
+    {
+        // Determine wether the slave should be sent left or right
+        if (leftWorkForce < rightWorkForce)
+        {
+            sc.targetPosition = leftZone.transform.position;
+        }
+        else
+        {
+            sc.targetPosition = rightZone.transform.position;
+        }
+    }
+
+}
