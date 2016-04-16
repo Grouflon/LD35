75be2589fbdc683f6c3648d9353191fee68b8627
Remi laptop <remi@uselessworks.com>
2016-04-16 19:08:18 +0200

    added recover time

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour {
 
     public GameObject chariotGO;
     public InputController inputController;
+    public float recoverTime;
 
 	void Start ()
     {
@@ -27,11 +28,18 @@ public class PlayerController : MonoBehaviour {
         }
         else
         {
-            m_currentPower = GameObject.Instantiate(m_powerManager.powerTemplates[0]);
-            m_currentPower.source = chariotGO;
+            m_recoverTimer += Time.deltaTime;
+
+            if (m_recoverTimer > recoverTime)
+            {
+                m_currentPower = GameObject.Instantiate(m_powerManager.powerTemplates[0]);
+                m_currentPower.source = chariotGO;
+                m_recoverTimer = 0.0f;
+            }
         }
 	}
 
+    private float m_recoverTimer = 0.0f;
     private PowerManager m_powerManager;
     private Power m_currentPower = null;
 }
