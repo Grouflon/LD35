183f9d32e4ebcb6fabf144c3ea574c3c167e53db
Remi laptop <remi@uselessworks.com>
2016-04-17 16:26:35 +0200

    Juicy fireball

---
+++ Assets/Scripts/GroundTargetController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundTargetController : MonoBehaviour {
+
+    public float angularSpeed = 0.0f;
+
+    public void FadeOut(float _duration)
+    {
+        GetComponent<FadeObjectInOut>().FadeOut(_duration);
+        Destroy(gameObject, _duration);
+    }
+
+	void Start()
+    {
+        Vector3 rotation = transform.rotation.eulerAngles;
+        rotation.y = Random.Range(0.0f, 360.0f);
+        transform.rotation = Quaternion.Euler(rotation);
+    }
+	
+	void Update()
+    {
+        Vector3 rotation = transform.rotation.eulerAngles;
+        rotation.y += angularSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(rotation);
+    }
+}
--- Assets/Scripts/SlaveController.cs
+++ Assets/Scripts/SlaveController.cs
@@ -7,6 +7,8 @@ public class SlaveController : MonoBehaviour
     public ChariotController masterChariot;
     public int strength;
     public int isWorking;
+    public float deathTime = 3.0f;
+    public float deathStrength = 5.0f;
 
     void Start()
     {
@@ -39,31 +41,50 @@ public class SlaveController : MonoBehaviour
 
     void MoveSlaveAss(Vector3 target)
     {
-        // Set Move direction
-        Vector3 slavePosition = this.transform.position;
-        Vector3 newDirection = target - slavePosition;
-        newDirection.y = 0f;
+        if (!m_isDead)
+        {
+            // Set Move direction
+            Vector3 slavePosition = this.transform.position;
+            Vector3 newDirection = target - slavePosition;
+            newDirection.y = 0f;
 
-        // Compute new position
-        newDirection.Normalize();
-        newDirection.z = newDirection.z * strength * Time.deltaTime;
-        newDirection.z += Random.Range(-0.6f, 0.6f) * newDirection.z;
-        newDirection.x = newDirection.x * strength * Time.deltaTime;
-        newDirection.x += Random.Range(-0.6f, 0.6f) * newDirection.x;
-        slavePosition += newDirection;
+            // Compute new position
+            newDirection.Normalize();
+            newDirection.z = newDirection.z * strength * Time.deltaTime;
+            newDirection.z += Random.Range(-0.6f, 0.6f) * newDirection.z;
+            newDirection.x = newDirection.x * strength * Time.deltaTime;
+            newDirection.x += Random.Range(-0.6f, 0.6f) * newDirection.x;
+            slavePosition += newDirection;
 
-        // Go there
-        this.transform.position = slavePosition;
+            // Go there
+            this.transform.position = slavePosition;
+        }
     }
 
 
-    void DestroySlave()
+    void DestroySlave(Vector3 _direction)
     {
-        Destroy(this.gameObject, 0.3f);
+        m_isDead = true;
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.isKinematic = false;
+        rigidbody.useGravity = true;
+        rigidbody.AddForceAtPosition(_direction * deathStrength, _direction * 0.01f, ForceMode.Impulse);
+
+        gameObject.GetComponentInChildren<Billboard>().enabled = false;
+
+        Destroy(this.gameObject, deathTime);
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (LayerMask.LayerToName(other.gameObject.layer) == "Lethal")
+        {
+            Vector3 direction = transform.position - other.transform.position;
+            if (direction.y < 0) direction.y = -direction.y;
+            direction.Normalize();
+            DestroySlave(direction);
+        }
+    }
 
-
-
-
+    private bool m_isDead = false;
 }
