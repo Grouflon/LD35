724012f44083d222e50c301e0754f521a3baaff1
Maxime Ferrieu <maxime.ferrieu@gmail.com>
2016-04-30 03:05:11 +0200

    [AUDIO] SoundbankManager creation

---
+++ Assets/Scripts/Audio/SoundbankManager/Soundbank.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Soundbank : MonoBehaviour 
+{
+	public string name;
+	public List<AudioClip> audioClips;
+	public bool isLoaded = false;
+
+	private int loadedClipSuccesCount = 0;
+	private int cycleCount = 0;
+
+	// Loads the soundbank
+	public void LoadSoundbank()
+	{
+		if (!isLoaded) 
+		{
+			foreach (AudioClip clip in audioClips) 
+			{
+				StartCoroutine (LoadAudioClip (clip));
+			}
+		}
+		else 
+		{
+			Debug.LogError ("The Soundbank " + name + " your're trying to load is already loaded");
+		}
+	}
+
+	// Handles the loading logic and waits for the files to be actually loaded before callback
+	IEnumerator LoadAudioClip (AudioClip clip)
+	{
+		
+		clip.LoadAudioData ();
+		while (clip.loadState == AudioDataLoadState.Loading || clip.loadState == AudioDataLoadState.Loading) 
+		{
+			yield return null;
+		}
+			
+		if (clip.loadState == AudioDataLoadState.Loaded) 
+		{				
+			loadedClipSuccesCount++;
+			cycleCount++;
+		}
+		else 
+		{
+			cycleCount++;
+		}
+	
+		if (cycleCount == audioClips.Count) 
+		{
+			isLoaded = CheckLoad ();
+
+			if (isLoaded) 
+			{
+				Debug.Log ("Soundbank " + name + " is loaded");
+			} 
+			else 
+			{
+				Debug.LogError ("Loading soundbank " + name + " has failed");
+			}
+		}				
+	}
+		
+	// Unloads the soundbank
+	public void UnloadSoundbank () 
+	{
+		if (isLoaded) 
+		{
+			foreach (AudioClip clip in audioClips) 
+			{
+				StartCoroutine (UnloadAudioClip (clip));
+			}
+		} 
+		else 
+		{
+			Debug.LogError ("The Soundbank " + name + " your're trying to unload is already unloaded");
+		}
+	}
+
+	// Handles the unloading logic and waits for the files to be actually unloaded before callback
+	IEnumerator UnloadAudioClip (AudioClip clip)
+	{
+		clip.UnloadAudioData ();
+		while (clip.loadState == AudioDataLoadState.Loaded || clip.loadState == AudioDataLoadState.Loading) 
+		{
+			yield return null;
+		}
+
+		if (clip.loadState == AudioDataLoadState.Unloaded) 
+		{
+			loadedClipSuccesCount--;
+			cycleCount--;
+		} 
+		else 
+		{
+			cycleCount--;
+		}
+
+		if (cycleCount == 0) 
+		{
+			isLoaded = CheckLoad ();
+
+			if (!isLoaded) 
+			{
+				Debug.Log ("Soundbank " + name + " has been unloaded");
+			} 
+			else 
+			{
+				Debug.LogError ("Unloading soundbank " + name + " has failed");
+			}
+		}				
+	} 
+
+
+	// Checks if the bank has been loaded or unloaded
+	bool CheckLoad()
+	{
+		bool bankIsLoaded = cycleCount == loadedClipSuccesCount && loadedClipSuccesCount == audioClips.Count;
+		return bankIsLoaded;
+	}
+
+	// Test Keys
+	/*void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			LoadSoundbank ();	
+		}
+		if (Input.GetKeyDown(KeyCode.Z))
+		{
+			UnloadSoundbank ();	
+		}
+	}*/
+}
---
+++ Assets/Scripts/Audio/SoundbankManager/SoundbankManager.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class SoundbankManager : MonoBehaviour {
+
+	public static SoundbankManager instance = null;
+	private Soundbank[] soundbanks;
+
+	void Awake () 
+	{
+		//Singleton pattern
+		if (instance == null) 
+		{
+			instance = this;
+		} 
+		else if (instance != null) 
+		{
+			Destroy (this.gameObject);
+		}
+		DontDestroyOnLoad (gameObject);
+
+		// get references from all the soundbanks
+		soundbanks = GetComponents<Soundbank> ();
+	}
+
+	// Loads the soundbank with a given name
+	public void Load(string soundbankName)
+	{
+		Soundbank soundbank = GetSoundbankWithName (soundbankName);
+		if (soundbank != null) 
+		{
+			soundbank.LoadSoundbank ();
+		}
+	}
+
+	// Unloads the soundbank with a given name
+	public void Unload(string soundbankName)
+	{
+		Soundbank soundbank = GetSoundbankWithName (soundbankName);
+		if (soundbank != null)
+		{
+			soundbank.UnloadSoundbank ();
+		}
+	}
+
+	// Checks if a soundbank with a given name is loaded
+	public bool IsSoundbankLoaded(string soundbankName)
+	{
+		Soundbank soundbank = GetSoundbankWithName (soundbankName);
+		if (soundbank != null) 
+		{
+			return soundbank.isLoaded;
+		} 
+		else 
+		{
+			Debug.LogError ("The Soundbank " + soundbankName + " your're trying to load does not exist.");
+			return false;
+		}
+	
+	}
+
+	// Unloads all the curently loaded soundbanks
+	public void UnloadAll()
+	{
+		foreach (Soundbank soundbank in soundbanks) 
+		{
+			if (soundbank.isLoaded) 
+			{
+				soundbank.UnloadSoundbank ();
+			}				
+		}
+	}
+
+	// Returns the reference of a soundbank with a given name
+	Soundbank GetSoundbankWithName(string name)
+	{
+		for (int i = 0; i < soundbanks.Length; i++) 
+		{
+			if (soundbanks [i].name == name) 
+			{
+				return soundbanks [i];
+			} 
+		}
+
+		Debug.LogError ("The Soundbank " + name + " your're trying to load does not exist.");
+		return null;
+	}
+}
